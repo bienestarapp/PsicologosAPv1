b869130d7a2f0ce6aeebfe1afde9e9955dd86a7c
bienestarapp <bienestarapp.cl@gmail.com>
2026-08-07 18:26:34 -0400

    Update migration dependency for suggested price model

--- back/pricing/migrations/0005_alter_suggestedprice_options_suggestedprice_user_and_more.py
+++ back/pricing/migrations/0005_alter_suggestedprice_options_suggestedprice_user_and_more.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("profiles", "0017_remove_psychologistprofile_experience_description_and_more"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("pricing", "0004_create_suggestedprice_table"),
+        ("pricing", "0003_alter_suggestedprice_table"),
     ]
 
     operations = [
