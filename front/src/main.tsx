f623cce06f80d349c0207e88f4745c31e1d06c08
bienestarapp <bienestarapp.cl@gmail.com>
2026-08-08 01:16:58 -0400

    Integrate axios for API handling in main.tsx
    Added axios for API requests and configured base URL.

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from 'react' import { createRoot } from 'react-dom/client' import axios from 'axios' import './index.css' import App from './App.tsx' const apiUrl = import.meta.env.VITE_API_URL || '' axios.defaults.baseURL = apiUrl.replace(/\/api\/?$/, '') createRoot(document.getElementById('root')!).render( <StrictMode> <App /> </StrictMode>, )
